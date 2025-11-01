7baaca3060b0c4c7e7f6851362b7b33dd5655d15
GhMilen <gherghemilen@gmail.com>
2025-11-01 14:57:49 +0200

    Added more reusability to StarRating component. Practiced default values and User useing API scenario

--- src/index.js
+++ src/index.js
@@ -8,6 +8,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating></StarRating>
+    <StarRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Ok", "Good", "Great!"]}
+    ></StarRating>
+    <StarRating maxRating={5} color="red" size={24} className="test"></StarRating>
+    <StarRating maxRating={5} color="green" size={16} defaultRating={3}></StarRating>
   </React.StrictMode>
 );
